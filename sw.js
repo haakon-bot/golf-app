c55f42fc9e4a9ec1163966b54ed36f98a47f06bc
Haakon Lind <haakonlind@gmail.com>
2026-05-26 11:37:06 +0200

    deploy v1.114 – 2026-05-26 11:37

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fore-v107';
+const CACHE = 'fore-v108';
 const PRECACHE = ['./', './styles.css', './app.js', './courses.js', './players.js', './rounds.js', './live.js', './profile.js', './scoring.js'];
 
 self.addEventListener('install', e => {
